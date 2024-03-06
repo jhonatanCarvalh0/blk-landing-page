f17e182f8143c460962b8ee46455c38ca0f0b969
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2024-03-06 16:25:26 -0400

    -

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import {Header} from './components/header/Header.js'
 import {Section} from "./components/section/Section.js";
 import {Footer} from "./components/footer/Footer.js";
+import {Navbar} from "./components/navbar/navbar.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML += Navbar()
     document.body.innerHTML += Header()
     document.body.innerHTML += Section('inicio')
     document.body.innerHTML += Section('aboutUs')
