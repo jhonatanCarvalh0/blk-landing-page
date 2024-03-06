9c7e5c3948f1a2bec2188ea8f5b1c979ce46b4b3
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2024-03-06 17:51:43 -0400

    Seção 2 - Sobre Nós
    Design inicial aplicado e preparado para receber os textos e fotos;

--- components/section/section-tailwind2.js
+++ components/section/section-tailwind2.js
@@ -5,13 +5,16 @@ export function sectionTailwind2() {
     <section id="about-us" class="bg-white dark:bg-gray-900">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
-            <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+<!--TITULO DA SEÇÃO-->
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            Sobre Nós
+            </h2>
+            <p class="mb-4">[TEXTO DE APRESENTAÇÃO DA EQUIPE]</p>
+            <p>[OUTRO TEXTO DE APRESENTAÇÃO DA EQUIPE]</p>
         </div>
         <div class="grid grid-cols-2 gap-4 mt-8">
-            <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
-            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+            <img class="w-full rounded-lg" src="https://img.freepik.com/fotos-gratis/homem-construtor-de-tiro-medio-olhando-para-longe_23-2148751955.jpg?t=st=1709761698~exp=1709765298~hmac=25e183c3322772ad09cd7cb7603dc11c5a8f58e3145eca0f3454ea6b3947d22b&w=740" alt="office content 1">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://img.freepik.com/fotos-gratis/imagem-de-homem-bonito-posando-no-estudio_613910-7500.jpg?t=st=1709761726~exp=1709765326~hmac=d97e81af9ca91a85a3c0a94a3b08aacc5fdb04b2534c0dea53069871d76bf4af&w=740" alt="office content 2">
         </div>
     </div>
 </section>
