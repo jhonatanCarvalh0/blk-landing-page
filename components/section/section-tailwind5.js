f9439595db286341fb34b3599c306518e044252b
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2024-03-07 13:06:18 -0400

    general fix

--- components/section/section-tailwind5.js
+++ components/section/section-tailwind5.js
@@ -24,7 +24,7 @@ export function sectionTailwind5(showSectionSeparator){
                   <label for="message" class="block mb-2 text-sm font-medium text-zinc-950 dark:text-gray-400">Sua mensagem</label>
                   <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-zinc-950 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Mensagem"></textarea>
               </div>
-              <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar Mensagem</button>
+              <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-zinc-700 sm:w-fit hover:bg-zinc-100 hover:text-zinc-950 focus:ring-4 focus:outline-none focus:ring-zinc-300 dark:bg-zinc-600 dark:hover:bg-zinc-100  dark:focus:ring-zinc-800">Enviar Mensagem</button>
           </form>
       </div>
 </section>
