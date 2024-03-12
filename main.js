a7db2796a0dfbcaa46eb63cc8f5f88afa659b032
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2024-03-12 12:20:49 -0400

    -

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ import {gallery} from "./components/section/gallery.js";
 import {navbar} from "./components/navbar/navbar.js";
 import {contact} from "./components/section/contact.js";
 import {footer} from "./components/footer/footer.js";
+import {ourTeam} from "./components/section/ourTeam.js";
 
 const showSectionSeparator = false;
 document.addEventListener('DOMContentLoaded', () => {
     document.body.innerHTML += navbar(showSectionSeparator)  // navbar
     document.body.innerHTML += header(showSectionSeparator) // hero page
     document.body.innerHTML += aboutUs(showSectionSeparator) // about us
-    document.body.innerHTML += missionValue(showSectionSeparator) // mVValue page
+    document.body.innerHTML += missionValue(showSectionSeparator) // mission vision and value
+    document.body.innerHTML += ourTeam(showSectionSeparator) // our team
     document.body.innerHTML += ourServices(showSectionSeparator) // services
     document.body.innerHTML += gallery(showSectionSeparator) // gallery
     document.body.innerHTML += contact(showSectionSeparator) // contact
