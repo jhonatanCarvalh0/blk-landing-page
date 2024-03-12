aab1320199aedcab0d2be9989b319ace0b3aca34
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2024-03-12 09:24:38 -0400

    funtion optimization

--- components/section/gallery.js
+++ components/section/gallery.js
@@ -9,48 +9,24 @@ export function gallery(showSectionSeparator) {
 ${tag_filters()}
 <section id="gallery" class="grid grid-cols-2 px-6 md:grid-cols-4 gap-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 h-full dark:bg-zinc-950 dark:text-white"">
     <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
-        </div>
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg')}
     </div>
     <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
-        </div>
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg')}
     </div>
     <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
-        </div>
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg')}
     </div>
     <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
-        </div>
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg')}
+        ${image_add('https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg')}
     </div>
 </section>
 
@@ -74,3 +50,10 @@ function tag_filters(){
   `
 }
 
+function image_add(srcURL){
+    return `
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src=${srcURL} alt="">
+        </div>
+    `
+}
