731b6927d37541a6b12f2a22f933723b13aae4f5
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-03-12 08:25:43 -0400

    feat

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import {sectionTailwind1} from "./components/section/section-tailwind1.js";
-import {sectionTailwind3} from "./components/section/section-tailwind3.js";
 import {sectionTailwind2} from "./components/section/section-tailwind2.js";
+import {sectionTailwind3} from "./components/section/section-tailwind3.js";
 import {sectionTailwind4} from "./components/section/section-tailwind4.js";
+import {sectionTailwind5} from "./components/section/section-tailwind5.js";
 import {footerTailwind1} from "./components/footer/footer-tailwind1.js";
 import {navbarTailwind1} from "./components/navbar/navbar-tailwind1.js";
-import {sectionTailwind5} from "./components/section/section-tailwind5.js";
 import {sectionViewSeparator} from "./components/sectionViewSeparator.js";
 
 const showSectionSeparator = false;
